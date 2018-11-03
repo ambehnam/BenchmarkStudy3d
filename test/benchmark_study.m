123dd9247bfccf95f461137eb112bff2c6bc4ab5
ambehnam <a.f.behnam@gmail.com>
2018-11-02 23:17:47 -0400

    run for errors

--- test/benchmark_study.m
+++ test/benchmark_study.m
@@ -1,11 +1,15 @@
 clc ; clear all,close all;
 
-study = benchmark_study3d('RCFT');
+study = benchmark_study3d('SRC');
 % study.touch_data(1);
 study.run_benchmark_study_3d; 
-% check = study.check_data;
-% study.unconservative_error_cat;
+study.merge_results;
+check = study.check_data;
 results = study.get_study_results;
+study.unconservative_error_cat;
+error_accepted_as_real = 10;
+study.run_benchmark_error_list(error_accepted_as_real);
 % 
-% hold all
-% study.figure(1,'surface1')
+hold all
+study.figure(14,'surface1')
+study.figure(14,'surface4')
